e0e7c1b3b1a0977dfad0cab270755c23d1b49b1a
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2024-05-06 17:52:13 +0800

    Bugfixes

--- src/main.ts
+++ src/main.ts
@@ -14,36 +14,42 @@ import Swal from 'sweetalert2'
 if (window.location.pathname !== '/') window.location.href = '/'
 if (window.location.hash !== '') window.location.hash = ''
 
-Database.instance.init().then(() => {
-    document.body.style.opacity = '1'
+const a = (document.getElementById('to-admin') as HTMLAnchorElement)
+const href = a.href
 
-    if (debug.bypass_ws) Router.instance.init()
-    
-    else 
-    Scanner.instance.init('192.168.1.1', 6969)
-        .then(msg => {
-            console.log(msg)
-            Router.instance.init()
-        })
-    
-        .catch(err => {
-            console.error(err)
-    
-            Swal.fire({
-                icon: 'error',
-                html: 'The application cannot find the scanner.<br>Run the app anyway?',
-                allowEscapeKey: true,
-                allowOutsideClick: false,
-                showCancelButton: true,
+a.href = ''
 
-                confirmButtonText: 'Run',
-                cancelButtonText: 'Exit App'
-            }).then(result => {
-                if (result.isConfirmed)
-                    Router.instance.init()
+Database.instance.init().then(() => {    
+	document.getElementById('to-admin')?.style.setProperty('pointer-events', '')
+	document.body.style.opacity = '1'
+
+	if (debug.bypass_ws) Router.instance.init()
+	else
+		Scanner.instance
+			.init('192.168.1.1', 6969)
+			.then(msg => {
+				console.log(msg)
                 
-                else
-                    exit(1)
-            })
-        })
+				Router.instance.init()
+			})
+
+			.catch(err => {
+				console.error(err)
+
+				Swal.fire({
+					icon: 'error',
+					html: 'The application cannot find the scanner.<br>Run the app anyway?',
+					allowEscapeKey: true,
+					allowOutsideClick: false,
+					showCancelButton: true,
+
+					confirmButtonText: 'Run',
+					cancelButtonText: 'Exit App',
+				}).then(result => {
+					if (result.isConfirmed) Router.instance.init()
+					else exit(1)
+				})
+			})
+            
+            .finally(() => a.href = href)
 })
